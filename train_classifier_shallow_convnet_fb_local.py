f618b4c59515da7c8a8cab71ca2825bd172c27b9
Jacob <liaojxsir@gmail.com>
2018-04-20 09:00:19 +0800

    local

--- train_classifier_shallow_convnet_fb_local.py
+++ train_classifier_shallow_convnet_fb_local.py
@@ -97,7 +97,7 @@ def train(X_train, y_train, X_val, y_val, subject):
         pipe1 = LeakyReLU(alpha=0.05)(pipe1)
         pipe1 = Dropout(0.5)(pipe1)
         pipe1 = BatchNormalization()(pipe1)
-        #pipe1 = Reshape((pipe1.shape[1].value, 42, 4))(pipe1)
+        pipe1 = Reshape((pipe1.shape[1].value, 4))(pipe1)
         
         """
         pipe2 = Conv3D(40, (1,3,3), strides=(1,1,1), padding='same')(inputs)
