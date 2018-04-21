e9eda93cabcd4bf6f9b18d78cda2e1ecdca7fb03
Jacob <liaojxsir@gmail.com>
2018-04-21 20:00:23 +0800

    a

--- train_classifier_shallow_convnet_fb_local.py
+++ train_classifier_shallow_convnet_fb_local.py
@@ -111,8 +111,8 @@ def train(X_train, y_train, X_val, y_val, subject):
         pipe2 = Dropout(0.5)(pipe2)
         pipe2 = BatchNormalization()(pipe2)
         pipe2 = se_block(pipe2, compress_rate=8)
-        pipe2 = Conv3D(4, (1,1,1), strides=(1,1,1), padding='valid')(pipe2)
-        pipe2 = Reshape((pipe2.shape[1].value, 4))(pipe2)
+        #pipe2 = Conv3D(4, (1,1,1), strides=(1,1,1), padding='valid')(pipe2)
+        pipe2 = Reshape((pipe2.shape[1].value, 40))(pipe2)
         
         """
         pipe12 = concatenate([pipe1,pipe2], axis=4)
