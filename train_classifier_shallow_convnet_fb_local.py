9a901498f8fe188e9425537beff407dbde2a4e7d
Jiexun liao <diasporado>
2018-04-22 22:31:48 +0800

    'asd'

--- train_classifier_shallow_convnet_fb_local.py
+++ train_classifier_shallow_convnet_fb_local.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 from keras.models import Model, Sequential, load_model
 from keras.layers import Dense,BatchNormalization,AveragePooling2D,MaxPooling2D,MaxPooling3D, \
     Convolution2D,Activation,Flatten,Dropout,Convolution1D,Reshape,Conv3D,TimeDistributed,LSTM,AveragePooling3D, \
-    Input, AveragePooling3D, MaxPooling3D, concatenate, LeakyReLU, AveragePooling1D, GlobalAveragePooling1D, \
+    Input, AveragePooling3D, MaxPooling3D, concatenate, LeakyReLU, AveragePooling1D, GlobalAveragePooling3D, \
     multiply
 from keras.utils.np_utils import to_categorical
 from keras import optimizers, callbacks
@@ -26,7 +26,7 @@ def se_block(input_tensor, compress_rate = 4):
     num_channels = int(input_tensor.shape[-1]) # Tensorflow backend
     bottle_neck = int(num_channels//compress_rate)
  
-    se_branch = GlobalAveragePooling1D()(input_tensor)
+    se_branch = GlobalAveragePooling3D()(input_tensor)
     se_branch = Dense(bottle_neck, activation='relu')(se_branch)
     se_branch = Dense(num_channels, activation='sigmoid')(se_branch)
  
@@ -105,12 +105,14 @@ def train(X_train, y_train, X_val, y_val, subject):
         #pipe2 = LeakyReLU(alpha=0.05)(pipe2)
         #pipe2 = Dropout(0.5)(pipe2)
         #pipe2 = BatchNormalization()(pipe2)
-        #pipe2 = se_block(pipe2, compress_rate=4)
+        pipe2 = se_block(pipe2, compress_rate=16)
         pipe2 = Conv3D(64, (1,3,3), strides=(1,1,1), padding='valid')(pipe2)
+        pipe2 = se_block(pipe2, compress_rate=16)
         pipe2 = Conv3D(64, (1,2,3), strides=(1,1,1), padding='valid')(pipe2)
         pipe2 = LeakyReLU(alpha=0.05)(pipe2)
         pipe2 = Dropout(0.5)(pipe2)
         pipe2 = BatchNormalization()(pipe2)
+        pipe2 = se_block(pipe2, compress_rate=16)
         #pipe2 = Conv3D(4, (1,1,1), strides=(1,1,1), padding='valid')(pipe2)
         pipe2 = Reshape((pipe2.shape[1].value, 64))(pipe2)
         #pipe2 = se_block(pipe2, compress_rate=16)
