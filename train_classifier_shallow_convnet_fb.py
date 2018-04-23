8175252152ada125447fe0b87b9bd57cac9d83cf
Jacob <liaojxsir@gmail.com>
2018-04-23 16:14:19 +0800

    a

--- train_classifier_shallow_convnet_fb.py
+++ train_classifier_shallow_convnet_fb.py
@@ -91,21 +91,21 @@ def train(X_train, y_train, X_val, y_val, subject):
     def layers(inputs):
         #pipe = Conv3D(40, (25,1,1), strides=(1,1,1), activation='linear')(inputs)
         
-        pipe1 = Conv3D(40, (1,3,3), strides=(1,1,1), padding='valid')(inputs)
-        pipe1 = Conv3D(40, (1,3,3), strides=(1,1,1), padding='valid')(pipe1)
-        pipe1 = Conv3D(40, (1,2,3), strides=(1,1,1), padding='valid')(pipe1)
+        pipe1 = Conv3D(64, (1,3,3), strides=(1,1,1), padding='valid')(inputs)
+        pipe1 = Conv3D(64, (1,3,3), strides=(1,1,1), padding='valid')(pipe1)
+        pipe1 = Conv3D(64, (1,2,3), strides=(1,1,1), padding='valid')(pipe1)
         pipe1 = LeakyReLU(alpha=0.05)(pipe1)
         pipe1 = Dropout(0.5)(pipe1)
         pipe1 = BatchNormalization()(pipe1)
-        pipe1 = Reshape((pipe1.shape[1].value, 40))(pipe1)
+        pipe1 = Reshape((pipe1.shape[1].value, 64))(pipe1)
         
-        pipe2 = Conv3D(40, (1,3,3), strides=(1,1,1), padding='valid')(inputs)
+        pipe2 = Conv3D(64, (1,3,3), strides=(1,1,1), padding='valid')(inputs)
         pipe2 = AveragePooling3D(pool_size=(1,3,3), strides=(1,1,1), padding='valid')(pipe2)
-        pipe2 = Conv3D(40, (1,2,3), strides=(1,1,1), padding='valid')(pipe2)
+        pipe2 = Conv3D(64, (1,2,3), strides=(1,1,1), padding='valid')(pipe2)
         pipe2 = LeakyReLU(alpha=0.05)(pipe2)
         pipe2 = Dropout(0.5)(pipe2)
         pipe2 = BatchNormalization()(pipe2)
-        pipe2 = Reshape((pipe2.shape[1].value, 40))(pipe2)
+        pipe2 = Reshape((pipe2.shape[1].value, 64))(pipe2)
         #pipe2 = se_block(pipe2, compress_rate=4)
         #pipe2 = Conv3D(64, (1,6,7), strides=(1,1,1), padding='valid')(pipe2)
         #pipe2 = BatchNormalization()(pipe2)
@@ -127,17 +127,17 @@ def train(X_train, y_train, X_val, y_val, subject):
         pipe12 = Conv3D(4, (1,1,1), strides=(1,1,1), padding='valid')(pipe12)
         pipe12 = Reshape((pipe12.shape[1].value, 4))(pipe12)
         """
-        pipe3 = Conv3D(40, (1,6,7), strides=(1,1,1), padding='valid')(inputs)
+        pipe3 = Conv3D(64, (1,6,7), strides=(1,1,1), padding='valid')(inputs)
         pipe3 = LeakyReLU(alpha=0.05)(pipe3)
         pipe3 = Dropout(0.5)(pipe3)
         pipe3 = BatchNormalization()(pipe3)
         #pipe3 = se_block(pipe3, compress_rate=16)
         #pipe3 = Conv3D(4, (1,1,1),  strides=(1,1,1), padding='valid')(pipe3)
-        pipe3 = Reshape((pipe3.shape[1].value, 40))(pipe3)
+        pipe3 = Reshape((pipe3.shape[1].value, 64))(pipe3)
         
         pipe = concatenate([pipe1,pipe2,pipe3], axis=2)
         #pipe = Convolution1D(4, 25, strides=1, padding='valid')(pipe)
-        pipe = se_block(pipe, compress_rate=12)
+        pipe = se_block(pipe, compress_rate=16)
         pipe = AveragePooling1D(pool_size=(75), strides=(15))(pipe)
         #pipe = Dense(40)(pipe)
         #pipe = LeakyReLU(alpha=0.05)(pipe)
@@ -239,7 +239,7 @@ if __name__ == '__main__': # if this file is been run directly by Python
                     for i in range(len(subjects_test))]
 
     # Iterate training and test on each subject separately
-    for i in range(9):
+    for i in range(1,9):
         train_index = subj_train_order[i] 
         test_index = subj_test_order[i]
         np.random.seed(123)
